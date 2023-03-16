5cb6328a834a6a3fc80ea7d25c41f94d3202ef38
swapnamohanty <126213650+swapnamohanty@users.noreply.github.com>
2023-03-16 19:07:53 -0400

    First Commit

---
+++ module.py
@@ -0,0 +1,9 @@
+def sqr_foot(length:int, width:int):
+    return f'Square Footage of House: {length * width} sq.ft'
+
+import math
+def findCircum(radius:int):
+    return f'Circumferenece of Circle: {2*math.pi*radius} inch'
+
+def feet_inch(length:int):
+    return f'Inch :{length*12}'
